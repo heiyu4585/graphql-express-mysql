cbbbf2ccbcc96cddea5265117f3384df3bd969b8
wangning <wangning1@allinmd.cn>
2018-03-23 17:46:55 +0800

    删除无用数据

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var userDao = require('../dao/userDao');
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource users');
-});
-// 增加用户
-router.get('/addUser', function(req, res, next) {
-    userDao.add(req, res, next);
+    res.send('respond with a resource users');
 });
 
 
